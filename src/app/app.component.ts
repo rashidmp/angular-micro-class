424daf01e509b404956e3d5de0eedbe47146cf69
Muhammed Rashid M P <alrashid4star@gmail.com>
2023-06-02 12:27:03 +0530

    angular-micro-class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-app';
+  newName = '';
+
+  names = ['Binsha', 'Adhil Khan', 'Jibin Althaf', 'Mubashira'];
+
+  addName(newName: string) {
+    this.names.push(newName);
+  }
 }
